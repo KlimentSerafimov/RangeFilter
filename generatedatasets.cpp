f9cef9eebcf3c93185ae10a00c6c72ececc6e0c4
veronica <wairimumuriga@gmail.com>
2021-11-27 19:08:24 -0500

    changes

--- generatedatasets.cpp
+++ generatedatasets.cpp
@@ -66,8 +66,39 @@ int urls(){
     return 0;
 }
 
+int small_dataset(){
+    ifstream nameFileout;
+
+    ofstream datasetfile;
+    ofstream workloadfile;
+	datasetfile.open("data/small_data_dataset.txt");
+    workloadfile.open("data/small_data_workload.txt");
+
+	// nameFileout.open("..datasets/emails-validated-random-only-30-characters.txt.sorted");
+	nameFileout.open("data/small_dataset.txt");
+    string line;
+
+	while(std::getline(nameFileout, line)){
+
+        // double r = rand()/RAND_MAX;
+        double r = (rand() * 1.0) /RAND_MAX;
+
+		if (r > 0.5){
+            datasetfile<<line<<endl;
+		} else {
+            workloadfile<<line<<endl;
+		}
+    }
+    
+    nameFileout.close();
+    workloadfile.close();
+    datasetfile.close();
+    return 0;
+}
+
 int main(){
     // emails();
-    urls();
+    // urls();
+    small_dataset();
     return 0;
 }
--- multilevelbloom.cpp
+++ multilevelbloom.cpp
@@ -32,17 +32,22 @@
 #include "bloom/bloom_filter.hpp"
 
 using namespace std;
+vector<bloom_filter> bfs;
+vector<string> keys;
 
-int create_multi_level_bfs(vector<bloom_filter> bfs, vector<string> keys){
+int create_multi_level_bfs(){
 
 	// std::vector<bloom_filter> bfs;
 	ifstream nameFileout;
 	// change this to read from datasets dataset
-	nameFileout.open("data/emails_dataset.txt");
+	// nameFileout.open("data/emails_dataset.txt");
 	// nameFileout.open("data/urls_dataset.txt");
+	nameFileout.open("data/small_data_dataset.txt");
+
 	string line;
 
 	while(std::getline(nameFileout, line)){
+		// cout<<"line: "<<line<<endl;
 		keys.push_back(line);
 		// insert each prefix into each bloom filter
 		for (int i = 0; i < line.length(); i++){
@@ -53,9 +58,9 @@ int create_multi_level_bfs(vector<bloom_filter> bfs, vector<string> keys){
 
 				// bf initialiation code
 				// How many elements roughly do we expect to insert?
-				parameters.projected_element_count = 1000000; // use a million keys
+				parameters.projected_element_count = 500; // for small_dataset.txt
 				// Maximum tolerable false positive probability? (0,1)
-				parameters.false_positive_probability = 0.0001; // 1 in 10000
+				parameters.false_positive_probability = 0.5; // 1 in 10000
 				// Simple randomizer (optional)
 				parameters.random_seed = 0xA5A5A5A5;
 				if (!parameters){
@@ -69,8 +74,12 @@ int create_multi_level_bfs(vector<bloom_filter> bfs, vector<string> keys){
 			}
 			// add this prefix to bf
 			bfs[i].insert(line.substr(0, i + 1));
+			// cout<<"inserting into bloom filter "<<i<<": "<<line.substr(0, i + 1)<<endl;
 		}	
 	}
+	nameFileout.close();
+
+	cout<<"key vector size: "<<keys.size()<<endl;
 
    return 0;
 }
@@ -118,11 +127,12 @@ int query_binary_search(vector<string> keys, string left_key, string right_key){
 
  */
 
-int query(vector<bloom_filter> bfs, string left_key, string right_key){
-
+int bf_query(string left_key, string right_key){
+	cout<<"1"<<endl;
 	int i = 0;
 	while (left_key[i] == right_key[i]){
 		// if dataset does not contain a common prefix, retuurn false
+		cout<<left_key<<" and "<<left_key[i];
 		if (!bfs[i].contains(left_key.substr(0, i + 1))){
 			return 0;
 		}
@@ -132,9 +142,13 @@ int query(vector<bloom_filter> bfs, string left_key, string right_key){
 	// gotten past common substring;
 	// query left_key[i] .... right_key[i] on BF
 	// edge case: BF for a certain index doesn't exist
-	// bounds checking: make sure right_key[i] is within the a-z || A-Z bounds
+	// bounds checking: make sure right_key[i] is within the a-z || A-Z bound
+	cout<<"left "<<left_key[i]<<" and right "<< right_key[i]+1<<endl;
 	for (char ch = left_key[i]; ch < right_key[i] + 1; ch++){
+		cout<<"checking for "<<left_key.substr(0, i) + ch<<endl;
+
 		if (bfs[i].contains(left_key.substr(0, i) + ch)){
+
 			return 1;
 		}
 	}
@@ -143,19 +157,17 @@ int query(vector<bloom_filter> bfs, string left_key, string right_key){
 }
 
 
-int query_bloom_filters(vector<string> keys, vector<bloom_filter> bfs){
+int query_bloom_filters(){
 
 	ifstream nameFileout;
 	// change this to read from workload dataset
-	nameFileout.open("data/emails_workload.txt");
+	// nameFileout.open("data/emails_workload.txt");
 	// nameFileout.open("data/url_workload.txt");
-
+	nameFileout.open("/home/sparklab/RangeFilter/small_data_workload.txt");
 
 	string line;
-	std::vector<std::string> left_keys;
-	std::vector<std::string> right_keys;
-	string left_key = 0;
-	string right_key = 0;
+	string left_key;
+	string right_key;
 
 	int fp = 0;
 	int tn = 0;
@@ -164,11 +176,10 @@ int query_bloom_filters(vector<string> keys, vector<bloom_filter> bfs){
 
 	while(std::getline(nameFileout, line)){
 		left_key = line;
-		right_key = left_key + left_key[left_key.length() - 1];	
-		left_keys.push_back(left_key);
-		right_keys.push_back(right_key);
+		right_key = left_key;
+		right_key[right_key.length() - 1]++;	
 
-		int bf_query_result = query(bfs, left_key, right_key);
+		int bf_query_result = bf_query(left_key, right_key);
 		int binary_search_result = query_binary_search(keys, left_key, right_key);
 
 		if (bf_query_result == 1 && binary_search_result == 0){
@@ -177,14 +188,15 @@ int query_bloom_filters(vector<string> keys, vector<bloom_filter> bfs){
 			tn += 1;
 		}
 	}
+	nameFileout.close();
 
-	fpr = fp / (fp + tn);
+	fpr = (fp * 1.0)/ (fp + tn);
 	cout<<"fpr: "<<fpr<<endl;
 	return 0;
 }
 
 
-int get_bf_memory(vector<bloom_filter> bfs){
+int get_bf_memory(){
 	int total_mem = 0;
 	for (int i = 0; i < bfs.size(); i++){
 		total_mem += bfs[i].get_memory();
@@ -194,13 +206,12 @@ int get_bf_memory(vector<bloom_filter> bfs){
 }
 
 int main(){
-	vector<bloom_filter> bfs;
-	vector<string> keys;
-	int return_code = create_multi_level_bfs(bfs, keys);
+
+	int return_code = create_multi_level_bfs();
 	cout<<"keys: "<<keys.size()<<endl;
 	
-	query_bloom_filters(keys, bfs);
-	get_bf_memory(bfs);
+	query_bloom_filters();
+	get_bf_memory();
 	
 	return 0;
 }
